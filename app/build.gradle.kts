6ef86c66d6f600a5cffae534becce73ba4809ba3
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2025-09-28 13:03:23 +0530

    Bump build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 500
-        versionName = "4.0.1"
+        versionName = "4.1.0"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh")
